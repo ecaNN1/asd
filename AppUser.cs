3003b4236ceaf4e6d0e5ff54ad28dcf7a8603c2b
ecaNN1 <129431360+ecaNN1@users.noreply.github.com>
2023-10-03 13:09:13 +0300

    Add files via upload

---
+++ AppUser.cs
@@ -0,0 +1,26 @@
+﻿using FinalProject.Core.Enums;
+using FinalProject.Core.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Core.Entities
+{
+    public class AppUser : IdentityUser, IBaseEntity
+    {
+        public string FullName { get; set; }
+        public string? ImagePath { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
+        public Status Status { get; set; }
+
+        //Navigation Prop
+        public virtual List<Post> Posts { get; set; }
+        public virtual List<Comment> Comments { get; set; }
+        public virtual List<Like> Likes { get; set; }
+    }
+}
